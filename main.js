3198affbd6db9998f0a9e59c4ecc45a8f2cdd1ab
Matthew Dulworth <mhdulworth@gmail.com>
2020-06-16 13:40:03 -0500

    Added basic matrix input creation

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+'use strict';
 
-let A = [
-   [1,1,1],
-   [1,1,1],
-   [9,9,9]
-];
+let submitBtn = document.querySelector("#submit button");
+let rowInput = document.querySelector("#rows");
+let columnInput = document.querySelector("#columns");
+let matrixWrapper = document.querySelector("#matrix-wrapper");
 
-let m = new Matrix(3,3, A);
+submitBtn.addEventListener('click', () => createMatrix(rowInput.value, columnInput.value));
 
-m.log();
-m.rowAddition(1,2,3);
-m.log();
+/**
+ * Creates an empty matrix and displays it on the screen.
+ * 
+ * @param {number} rows The number of rows.
+ * @param {number} columns The number of columns.
+ */
+function createMatrix(rows, columns) {
+   matrixWrapper.innerHTML = "";
+
+   for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+         matrixWrapper.appendChild(matrixEntrySpace(`${i},${j}`));
+      }
+   }
+
+   matrixWrapper.style.setProperty('grid-template-rows', `repeat(${rows}, auto)`);
+   matrixWrapper.style.setProperty('grid-template-columns', `repeat(${columns}, auto)`);
+}
+
+function matrixEntrySpace(id) {
+   let entrySpace = document.createElement('input');
+   entrySpace.type = 'text';
+   entrySpace.name = id;
+   entrySpace.id = id;
+   return entrySpace;
+}
