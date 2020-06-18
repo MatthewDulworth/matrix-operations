3bb0b3e5c4447697e79cb1a744de56b4385eb33d
Matthew Dulworth <mhdulworth@gmail.com>
2020-06-18 14:09:36 -0500

    Refactored Matrix class to be more functional

--- matrix.js
+++ matrix.js
@@ -2,6 +2,9 @@
 
 class Matrix {
 
+   // --------------------------------------------------------------------
+   // Instance Methods
+   // --------------------------------------------------------------------
    /**
     * Constructs an empty matrix with the specified number of rows and columns. 
     * 
@@ -11,15 +14,15 @@ class Matrix {
    constructor(rows, columns, inputMatrix) {
       this.rows = rows;
       this.columns = columns;
-      this.matrix = [];
+      this.array = [];
 
       if (inputMatrix !== undefined) {
-         this.matrix = inputMatrix;
+         this.array = inputMatrix;
       } else {
          for (let i = 0; i < rows; i++) {
-            this.matrix[i] = [];
+            this.array[i] = [];
             for (let j = 0; j < columns; j++) {
-               this.matrix[i][j] = undefined;
+               this.array[i][j] = undefined;
             }
          }
       }
@@ -34,53 +37,75 @@ class Matrix {
     * @param {number} column THe column of the entry.
     */
    updateEntry(row, column, value) {
-      this.matrix[row - 1][column - 1] = value;
+      this.array[row - 1][column - 1] = value;
    }
 
    /**
     * Outputs the matrix as a table to the console.
     */
    log() {
-      console.table(this.matrix);
+      console.table(this.array);
    }
 
+   // --------------------------------------------------------------------
+   // Row Operations
+   // --------------------------------------------------------------------
    /**
-    * Swaps the specified rows of the matrix. 
+    * Swaps the specified rows of a matrix. 
     * 
     * @param {number} targetRow A row to swap.
     * @param {number} actorRow A row to swap.
+    * @return {Matrix} The matrix with it's rows swapped.
     */
    rowSwap(targetRow, actorRow) {
+
+      // matrices count from 1, arrays count from 0
       targetRow -= 1;
       actorRow -= 1;
 
-      let tempRow = this.matrix[targetRow];
-      this.matrix[targetRow] = this.matrix[actorRow];
-      this.matrix[actorRow] = tempRow;
+      // return a new matrix with the rows swapped.
+      let swapped = this.array.map(row => row.slice());
+      swapped[targetRow] = this.array[actorRow].slice();
+      swapped[actorRow] = this.array[targetRow].slice();
+      return new Matrix(this.rows, this.columns, swapped);
    }
 
    /**
-    * Multiplies the specified row by the scalar.
+    * Multiplies a matrix row by a scalar.
     * 
     * @param {number} targetRow The row to multiply.
     * @param {number} scalar The value to multiply the row by. 
+    * @return {Matrix} Matrix with the row multiplied.
     */
-   rowMultiplication(targetRow, scalar) { 
+   rowMultiplication(targetRow, scalar) {
+
+      // matrices count from 1, arrays count from 0
       targetRow -= 1;
-      this.matrix[targetRow] = this.matrix[targetRow].map(entry => entry * scalar);
+
+      // return a new matrix with the specified row multiplied.
+      let multiplied = this.array.map(row => row.slice());
+      multiplied[targetRow] = this.array[targetRow].map(entry => entry * scalar);
+      return new Matrix(this.rows, this.columns, multiplied);
    }
 
    /**
-    * Adds the scalar multiply of the actor row to the target row.
+    * Adds a scalar multiple of a specified row to another. 
     * 
     * @param {number} targetRow The row to change. 
     * @param {number} actorRow The row to do the changing.
     * @param {number} scalar The multiple of the actor row.
+    * @return {Matrix} The resulting matrix. 
     */
-   rowAddition(targetRow, actorRow, scalar) { 
-      targetRow -=1;
-      actorRow -=1;
-      this.matrix[targetRow] = this.matrix[targetRow].map((entry, col) => entry - (scalar * this.matrix[actorRow][col]));
+   rowAddition(targetRow, actorRow, scalar) {
+
+      // matrices count from 1, arrays count from 0
+      targetRow -= 1;
+      actorRow -= 1;
+
+      // return the resulting matrix from the row addition. 
+      let result = this.array.map(row => row.slice());
+      result[targetRow] = this.array[targetRow].map((entry, col) => entry + (scalar * this.array[actorRow][col]));
+      return new Matrix(this.rows, this.columns, result);
    }
 
    matrixAddition(matrixB, subtraction) { }
@@ -92,9 +117,16 @@ class Matrix {
    inverse() { }
 }
 
-class IllegalArgumentException extends Error {
-   constructor(message) {
-      super(message);
-      this.name = this.constructor.name;
-   }
-}
+// testing
+
+// let m = [
+//    [1, 2, 3],
+//    [4, 5, 6],
+//    [7, 8, 9]
+// ];
+
+// let matrix = new Matrix(3, 3, m);
+// matrix.log();
+// let x = matrix.rowAddition(1,2,-1);
+// x.log();
+// matrix.log();
