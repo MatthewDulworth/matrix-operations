f9d802bc6a6819fb04d7623ef5abf0bc776e1f5c
Matthew Dulworth <mhdulworth@gmail.com>
2020-06-20 07:49:10 -0500

    in progress

--- matrix.js
+++ matrix.js
@@ -1,172 +1,193 @@
-'use strict';
+// 'use strict';
 
-class Matrix {
+// if (!Array.prototype.last){
+//    Array.prototype.last = function(){
+//        return this[this.length - 1];
+//    };
+// };
 
-   // --------------------------------------------------------------------
-   // Instance Methods
-   // --------------------------------------------------------------------
-   /**
-    * Constructs an empty matrix with the specified number of rows and columns. 
-    * 
-    * @param {number} rows The number of rows the matrix has.
-    * @param {number} columns The number of columns the matrix has.
-    */
-   constructor(rows, columns, inputMatrix) {
-      this.rows = rows;
-      this.columns = columns;
-      this.array = [];
+// class Matrix {
 
-      if (inputMatrix !== undefined) {
-         this.array = inputMatrix;
-      } else {
-         for (let i = 0; i < rows; i++) {
-            this.array[i] = [];
-            for (let j = 0; j < columns; j++) {
-               this.array[i][j] = undefined;
-            }
-         }
-      }
-   }
+//    // --------------------------------------------------------------------
+//    // Instance Methods
+//    // --------------------------------------------------------------------
+//    /**
+//     * Constructs an empty matrix with the specified number of rows and columns. 
+//     * 
+//     * @param {number} rows The number of rows the matrix has.
+//     * @param {number} columns The number of columns the matrix has.
+//     */
+//    constructor(rows, columns, inputMatrix) {
+//       this.rows = rows;
+//       this.columns = columns;
+//       this.array = [];
 
-   /**
-    * Updates the corresponding entry with the passed value. If the value is a string, 
-    * it will be treated as a variable. 
-    * 
-    * @param {number|string} value The value of the entry.
-    * @param {number} row The row of the entry.
-    * @param {number} column THe column of the entry.
-    */
-   updateEntry(row, column, value) {
-      this.array[row - 1][column - 1] = value;
-   }
+//       if (inputMatrix !== undefined) {
+//          this.array = inputMatrix;
+//       } else {
+//          for (let i = 0; i < rows; i++) {
+//             this.array[i] = [];
+//             for (let j = 0; j < columns; j++) {
+//                this.array[i][j] = undefined;
+//             }
+//          }
+//       }
+//    }
 
-   /**
-    * Outputs the matrix as a table to the console.
-    */
-   log() {
-      console.table(this.array);
-   }
+//    /**
+//     * Updates the corresponding entry with the passed value. If the value is a string, 
+//     * it will be treated as a variable. 
+//     * 
+//     * @param {number|string} value The value of the entry.
+//     * @param {number} row The row of the entry.
+//     * @param {number} column THe column of the entry.
+//     */
+//    updateEntry(row, column, value) {
+//       this.array[row - 1][column - 1] = value;
+//    }
 
-   // --------------------------------------------------------------------
-   // Row Operations
-   // --------------------------------------------------------------------
-   /**
-    * Swaps the specified rows of a matrix. 
-    * 
-    * @param {number} targetRow A row to swap.
-    * @param {number} actorRow A row to swap.
-    * @return {Matrix} The matrix with it's rows swapped.
-    */
-   rowSwap(targetRow, actorRow) {
+//    /**
+//     * Outputs the matrix as a table to the console.
+//     */
+//    log() {
+//       console.table(this.array);
+//    }
 
-      // matrices count from 1, arrays count from 0
-      targetRow -= 1;
-      actorRow -= 1;
+//    // --------------------------------------------------------------------
+//    // Row Operations
+//    // --------------------------------------------------------------------
+//    /**
+//     * Swaps the specified rows of a matrix. 
+//     * 
+//     * @param {number} targetRow A row to swap.
+//     * @param {number} actorRow A row to swap.
+//     * @return {Matrix} The matrix with it's rows swapped.
+//     */
+//    rowSwap(targetRow, actorRow) {
 
-      // return a new matrix with the rows swapped.
-      let swapped = this.array.map(row => row.slice());
-      swapped[targetRow] = this.array[actorRow].slice();
-      swapped[actorRow] = this.array[targetRow].slice();
-      return new Matrix(this.rows, this.columns, swapped);
-   }
+//       // matrices count from 1, arrays count from 0
+//       targetRow -= 1;
+//       actorRow -= 1;
 
-   /**
-    * Multiplies a matrix row by a scalar.
-    * 
-    * @param {number} targetRow The row to multiply.
-    * @param {number} scalar The value to multiply the row by. 
-    * @return {Matrix} Matrix with the row multiplied.
-    */
-   rowMultiplication(targetRow, scalar) {
+//       // return a new matrix with the rows swapped.
+//       let swapped = this.array.map(row => row.slice());
+//       swapped[targetRow] = this.array[actorRow].slice();
+//       swapped[actorRow] = this.array[targetRow].slice();
+//       return new Matrix(this.rows, this.columns, swapped);
+//    }
 
-      // matrices count from 1, arrays count from 0
-      targetRow -= 1;
+//    /**
+//     * Multiplies a matrix row by a scalar.
+//     * 
+//     * @param {number} targetRow The row to multiply.
+//     * @param {number} scalar The value to multiply the row by. 
+//     * @return {Matrix} Matrix with the row multiplied.
+//     */
+//    rowMultiplication(targetRow, scalar) {
 
-      // return a new matrix with the specified row multiplied.
-      let multiplied = this.array.map(row => row.slice());
-      multiplied[targetRow] = this.array[targetRow].map(entry => entry * scalar);
-      return new Matrix(this.rows, this.columns, multiplied);
-   }
+//       // matrices count from 1, arrays count from 0
+//       targetRow -= 1;
 
-   /**
-    * Adds a scalar multiple of a specified row to another. 
-    * 
-    * @param {number} targetRow The row to change. 
-    * @param {number} actorRow The row to do the changing.
-    * @param {number} scalar The multiple of the actor row.
-    * @return {Matrix} The resulting matrix. 
-    */
-   rowAddition(targetRow, actorRow, scalar) {
+//       // return a new matrix with the specified row multiplied.
+//       let multiplied = this.array.map(row => row.slice());
+//       multiplied[targetRow] = this.array[targetRow].map(entry => entry * scalar);
+//       return new Matrix(this.rows, this.columns, multiplied);
+//    }
 
-      // matrices count from 1, arrays count from 0
-      targetRow -= 1;
-      actorRow -= 1;
+//    /**
+//     * Adds a scalar multiple of a specified row to another. 
+//     * 
+//     * @param {number} targetRow The row to change. 
+//     * @param {number} actorRow The row to do the changing.
+//     * @param {number} scalar The multiple of the actor row.
+//     * @return {Matrix} The resulting matrix. 
+//     */
+//    rowAddition(targetRow, actorRow, scalar) {
 
-      // return the resulting matrix from the row addition. 
-      let result = this.array.map(row => row.slice());
-      result[targetRow] = this.array[targetRow].map((entry, col) => entry + (scalar * this.array[actorRow][col]));
-      return new Matrix(this.rows, this.columns, result);
-   }
+//       // matrices count from 1, arrays count from 0
+//       targetRow -= 1;
+//       actorRow -= 1;
 
-   // --------------------------------------------------------------------
-   // Row Reduction
-   // --------------------------------------------------------------------
+//       // return the resulting matrix from the row addition. 
+//       let result = this.array.map(row => row.slice());
+//       result[targetRow] = this.array[targetRow].map((entry, col) => entry + (scalar * this.array[actorRow][col]));
+//       return new Matrix(this.rows, this.columns, result);
+//    }
 
-   getColumn(column) {
-      return this.array.map(row => row[column - 1]);
-   }
+//    // --------------------------------------------------------------------
+//    // Row Reduction
+//    // --------------------------------------------------------------------
 
-   getRow(row){
-      return this.array[row - 1];
-   }
+//    getColumn(column) {
+//       return this.array.map(row => row[column - 1]);
+//    }
 
-   // rref(showSteps) {
-   //    let steps = [];
-   //    steps.push(this);
+//    getRow(row) {
+//       return this.array[row - 1];
+//    }
 
-   //    let column = 1;
-   //    for (let row = 1; row < this.rows; row++) {
+
+//    ref() {
+//       let steps = [];
+//       steps.push(this);
+
+//       let column = 1;
+//       for (let row = 1; row<=this.rows; row++){
          
-   //    }
+//          // swap to largest, if row,col is 0, j++
+//       }
 
-   //    if (showSteps) {
-   //       return steps;
-   //    } else {
-   //       return steps[steps.length];
-   //    }
-   // }
+//       return steps;
+//    }
+
+//    reduce() {
+      
+//    }
+   
+
+//    rref(showSteps) {
+//       let steps = [];
+      
+
+//       return steps;
+//    }
 
 
 
-   // --------------------------------------------------------------------
-   // Matrix Operations
-   // --------------------------------------------------------------------
-   sum(matrixB, subtraction) { }
-   product(matrixB) { }
-   equals(matrixB) { }
 
-   transpose() { }
-   determinate() { }
-   inverse() { }
-}
+//    // --------------------------------------------------------------------
+//    // Matrix Operations
+//    // --------------------------------------------------------------------
+//    sum(matrixB, subtraction) { }
+//    product(matrixB) { }
+//    equals(matrixB) { }
 
-// testing
+//    transpose() { }
+//    determinate() { }
+//    inverse() { }
+// }
 
-// let A = [
-//    [1, 2, 3],
-//    [4, 5, 6],
-//    [7, 8, 9]
-// ];
-// let matrixA = new Matrix(3, 3, A);
+// // testing
 
-let B = [
-   [9, 8, 7],
-   [6, 5, 4],
-   [3, 2, 1]
-]
+// // let A = [
+// //    [1, 2, 3],
+// //    [4, 5, 6],
+// //    [7, 8, 9]
+// // ];
+// // let matrixA = new Matrix(3, 3, A);
 
-let matrixB = new Matrix(3, 3, B);
+// // let B = [
+// //    [9, 8, 7],
+// //    [6, 5, 4],
+// //    [3, 2, 1]
+// // ]
+// // let matrixB = new Matrix(3, 3, B);
 
-console.table(matrixB.array);
-console.table(matrixB.getColumn(2));
-console.table(matrixB.getRow(2));
+// let C = [
+//    [0, 0, 2, 0],
+//    [0, 0, 3, 9],
+//    [2, 2, 7, 0],
+//    [0, 0, 5, 9]
+// ]
+// let matrixC = new Matrix(3, 3, C);
+// let steps = matrixC.rref();
