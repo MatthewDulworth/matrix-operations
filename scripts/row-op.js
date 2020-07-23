8f701866bfa5e5123a2df9725f312e0624768e67
Matthew Dulworth <mhdulworth@gmail.com>
2020-07-23 15:52:35 -0500

    Added matrix setup

---
+++ scripts/row-op.js
@@ -0,0 +1,4 @@
+let createBtn = document.querySelector(".create-btn");
+let rowInput = document.querySelector(".row-in");
+let columnInput = document.querySelector(".col-in");
+let matrixWrapper = document.querySelector(".matrix-wrapper");
