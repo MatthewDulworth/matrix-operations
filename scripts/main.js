eaae8f5bab8f3cb924e57551651c8cc741667fd4
Matthew Dulworth <mhdulworth@gmail.com>
2020-07-23 18:01:35 -0500

    added row operations controls

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 let rowInputs = document.querySelectorAll(".row-in");
 let columnInputs = document.querySelectorAll(".col-in");
 let matrixWrappers = document.querySelectorAll(".matrix-wrapper");
+let rowLists = document.querySelectorAll(".row-list");
 let matrices = [];
 
 rowInputs.forEach((input, index) => input.addEventListener('change', () => createMatrixInput(index)));
@@ -34,6 +35,40 @@ function createMatrixInput(matrixIndex) {
 
    matrixWrapper.style.setProperty('grid-template-rows', `repeat(${rows}, auto)`);
    matrixWrapper.style.setProperty('grid-template-columns', `repeat(${columns}, auto)`);
+   createRowList(rows);
+}
+
+/**
+ * Generates the lists of rows to be used in selecting a row.
+ * 
+ * @param {number} rows 
+ */
+function createRowList(rows) {
+   if (rowLists.length > 0) {
+
+      let options = "";
+
+      for (let row = 1; row <= rows; row++) {
+         options += `<option value=${row}>${row}</option>`;
+      }
+      rowLists.forEach(list => list.innerHTML = options);
+   }
+}
+
+/**
+ * Creates a text input for the entry of the matrix at 
+ * 
+ * @param {number} row The row of the entry.
+ * @param {number} columns The column of the entry. 
+ */
+function matrixEntrySpace(row, column) {
+   let entrySpace = document.createElement('input');
+   entrySpace.type = 'text';
+   entrySpace.name = `${row},${column}`;
+   entrySpace.id = `${row},${column}`;
+   entrySpace.classList.add("entry");
+   entrySpace.addEventListener("focus", () => entrySpace.select());
+   return entrySpace;
 }
 
 /**
@@ -52,19 +87,4 @@ function populateMatrix(matrixIndex) {
          matrices[matrixIndex].updateEntry(i, j, val);
       }
    }
-}
-
-/**
- * Creates a text input for the entry of the matrix at 
- * 
- * @param {number} row The row of the entry.
- * @param {number} columns The column of the entry. 
- */
-function matrixEntrySpace(row, column) {
-   let entrySpace = document.createElement('input');
-   entrySpace.type = 'text';
-   entrySpace.name = `${row},${column}`;
-   entrySpace.id = `${row},${column}`;
-   entrySpace.classList.add("entry");
-   return entrySpace;
 }
