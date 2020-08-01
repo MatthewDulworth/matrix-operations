a0deb50f2e4672ec241642f08ed001313d8fcaf2
Matthew Dulworth <mhdulworth@gmail.com>
2020-08-01 16:54:44 -0500

    Create button functionality removed from input class, added to row-op-input.js

--- scripts/row-op-input.js
+++ scripts/row-op-input.js
@@ -5,3 +5,21 @@
 // -----------------------------------------------------------
 /**@type {MatrixInput} */
 const matrixInput = new MatrixInput("_0");
+matrixInput.createBtn.addEventListener('click', () => {
+
+   let success = true;
+   let matrix;
+
+   try {
+      matrix = matrixInput.toArray();
+   } catch (error) {
+      success = false;
+      alert("Please make sure your matrix input is limited to fractions or decimal numbers.");
+   }
+
+   if (success) {
+      sessionStorage.setItem(matrixInput.ID, JSON.stringify(matrix));
+      window.location.href = '../html/row-op.html';
+   }
+
+});
