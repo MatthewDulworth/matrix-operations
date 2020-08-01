15995fb871ba4383159d8efa87fe0167465ddbdb
Matthew Dulworth <mhdulworth@gmail.com>
2020-08-01 16:08:33 -0500

    refactored add create button listenr into the input matrix class

--- scripts/row-op-input.js
+++ scripts/row-op-input.js
@@ -4,16 +4,4 @@
 // Constants
 // -----------------------------------------------------------
 /**@type {MatrixInput} */
-const matrixInput = new MatrixInput("_0");; 
-
-/**@type {string[][]} */
-let matrix = [];
-
-matrixInput.createBtn.addEventListener('click', () => {
-   try {
-      matrix = matrixInput.toArray();
-      console.log(matrix);
-   } catch (error) {
-      console.log("fail");
-   }
-});
+const matrixInput = new MatrixInput("_0");
