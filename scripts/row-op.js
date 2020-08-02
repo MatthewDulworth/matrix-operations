e83a1140d7d73c214f18a967e93b8bb7be63bc22
Matthew Dulworth <mhdulworth@gmail.com>
2020-08-01 20:39:24 -0500

    refactored row-op into RowOperationsCalculator class

--- scripts/row-op.js
+++ scripts/row-op.js
@@ -1,60 +1,80 @@
 'use strict';
 
-// -----------------------------------------------------------
-// Constants
-// -----------------------------------------------------------
-const rowLists = document.querySelectorAll(".row-list");
-const initialMatrix = document.querySelector("#initial .matrix-wrapper");
-const finalMatrix = document.getElementById("#final .matrix-wrapper");
-const inputMatrixArray = JSON.parse(sessionStorage.getItem("_0"));
+class RowOperationsCalculator {
+   /**
+    * 
+    * @param {string} matrixID 
+    */
+   constructor(matrixID) {
+      this.rowLists = document.querySelectorAll(".row-list");
+      this.inputMatrixArray = JSON.parse(sessionStorage.getItem(matrixID));
 
-// matrix 
-let matrix = new Matrix(inputMatrixArray.length, inputMatrixArray[0].length, inputMatrixArray);
-console.log(matrix);
+      this.initialDisplayMatrix = document.querySelector("#initial .matrix-wrapper");
+      this.finalDisplayMatrix = document.querySelector("#final .matrix-wrapper");
 
-// -----------------------------------------------------------
-// Main
-// -----------------------------------------------------------
-generateDisplayMatrix(inputMatrixArray, initialMatrix);
+      this.multiplyBtn = document.getElementById("row-multiply");
+      this.addBtn = document.getElementById("row-replace");
+      this.swapBtn = document.getElementById("row-swap");
 
-// -----------------------------------------------------------
-// Functions
-// -----------------------------------------------------------
-/**
- * Generates the lists of rows to be used in selecting a row.
- * @param {NodeList} rowLists The select elements.
- * @param {HTMLElement} rowInput The row input for the matrix.
- */
-function createRowList(rowLists, rowInput) {
-   if (rowLists.length > 0) {
-      let rows = rowInput.value;
-      let options = "";
+      this.multiplyBtn.addEventListener('click', this.multiply);
+      this.addBtn.addEventListener('click', this.add);
+      this.swapBtn.addEventListener('click', this.swap);
+
+      this.displayMatrix(this.inputMatrixArray, this.initialDisplayMatrix);
+      this.initRowLists(this.rowLists, this.inputMatrixArray.length);
+   }
+
+   /**
+    * Generates the lists of rows to be used in selecting a row.
+    * @param {NodeList} rowLists The select elements.
+    * @param {number} rows The number of rows in the matrix.
+    */
+   initRowLists(rowLists, rows) {
+      let options = "<option value=''></option>";
       for (let row = 1; row <= rows; row++) {
          options += `<option value=${row}>${row}</option>`;
       }
       rowLists.forEach(list => list.innerHTML = options);
    }
-}
 
-/**
- * Displays the passed matrixArray on the screen. 
- * 
- * @param {string[][]} matrixArray The matrix to display.
- * @param {HTMLElement} displayMatrix The element to house the matrix.
- */
-function generateDisplayMatrix(matrix, displayMatrix) {
-   let rows = matrix.length;
-   let columns = matrix[0].length;
+   /**
+    * Displays the passed matrixArray on the screen. 
+    * 
+    * @param {string[][]} matrixArray The matrix to display.
+    * @param {HTMLElement} matrixWrapper The element to house the matrix.
+    */
+   displayMatrix(matrixArray, matrixWrapper) {
+      let rows = matrixArray.length;
+      let columns = matrixArray[0].length;
 
-   for(let row = 0; row<rows; row++){
-      for(let col=0; col<columns; col++){
-        let entry = document.createElement('div');
-        entry.textContent = matrix[row][col];
-        displayMatrix.appendChild(entry);
+      for (let row = 0; row < rows; row++) {
+         for (let col = 0; col < columns; col++) {
+            let entry = document.createElement('div');
+            entry.textContent = matrixArray[row][col];
+            matrixWrapper.appendChild(entry);
+         }
       }
+
+      matrixWrapper.parentElement.style.setProperty('display', 'block');
+      matrixWrapper.style.setProperty('grid-template-rows', `repeat(${rows}, auto)`);
+      matrixWrapper.style.setProperty('grid-template-columns', `repeat(${columns}, auto)`);
    }
 
-   displayMatrix.parentElement.style.setProperty('display', 'block');
-   displayMatrix.style.setProperty('grid-template-rows', `repeat(${rows}, auto)`);
-   displayMatrix.style.setProperty('grid-template-columns', `repeat(${columns}, auto)`);
+   multiply() {
+
+   }
+
+   add() {
+
+   }
+
+   swap() {
+
+   }
+
+   validateInput(str) {
+
+   }
 }
+
+const rowOpCalculator = new RowOperationsCalculator("_0");
