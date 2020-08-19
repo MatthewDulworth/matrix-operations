1ca95c39c7bdc4ebde25572c3b75ed084ac67bf3
Matthew Dulworth <mhdulworth@gmail.com>
2020-08-19 11:27:54 -0500

    Added init matrix selectors to matrix ops class

--- scripts/matrix-op.js
+++ scripts/matrix-op.js
@@ -9,8 +9,23 @@ class MatrixOpsCalculator {
     */
    constructor() {
       this.matrixInputs = this.initMatrices();
+      this.matrixSelects = document.querySelectorAll(".matrix-select");
+
+      // setup methods
+      this.setMatrixSelectValues();
    }
 
+   setMatrixSelectValues() {
+      // generate the matrix names based on the number of matrix inputs, e.g. A, B, C...
+      const names = this.matrixInputs.map((_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
+      
+      this.matrixSelects.forEach(select => {
+         let options = "";
+         names.forEach((name, i) => options += `<option value=${i}>${name}</option>`);
+         select.innerHTML = options;
+      });
+   }
+   
    /**
     * @returns 
     */
